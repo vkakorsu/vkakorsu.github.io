80240761d7b8ce32405c52b0d02c24c1c1da7daa
Vincent Kofi Akorsu <162510286+vkakorsu@users.noreply.github.com>
2025-04-19 14:24:16 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: "class", // Enable class-based dark mode
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
